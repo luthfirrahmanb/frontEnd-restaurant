87fb3b3287d81291bb1fa5b870aebb5c07b8dca9
Luthfir <luthfirrahman04@gmail.com>
2017-11-20 10:25:12 +0700

    adding logic for restaurant's open status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,7 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor(){
-    
+  today: any;
+  open: any;
+  closed: any;
+  restOpenStatus: boolean = false;
+  constructor() {
+
+    // this.RestaurantOpenStatus();
+
+
+  }
+  ngOnInit() {
+    //checking every 1 seconds the status of restaurant
+    setInterval(() => {
+      this.RestaurantOpenStatus();
+    }, 1000)
+  }
+
+  RestaurantOpenStatus() {
+    this.today = moment().format('hh:mm A');
+    this.open = '10:00 AM';
+    this.closed = '10:00 PM';
+    if (this.today >= this.open || this.today < this.closed) {
+      this.restOpenStatus = true;
+    } else {
+      this.restOpenStatus = false;
+    }
   }
 }
